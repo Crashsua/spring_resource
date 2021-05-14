5385833473f40c9517b3ef4685a62966540dbb75
Crashsua <tranngocthiena1@gmail.com>
2021-05-14 21:15:43 +0700

    Update service: resource

--- src/main/java/com/example/resource/service/resource.java
+++ src/main/java/com/example/resource/service/resource.java
@@ -2,6 +2,10 @@ package com.example.resource.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
 @Service
 public class resource {
+    private String resource;
+    private Date createDate;
 }
