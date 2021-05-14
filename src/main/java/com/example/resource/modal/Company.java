ec8a2009b72f26ee651dad5f8be9ca1d88d2b70d
Crashsua <tranngocthiena1@gmail.com>
2021-05-14 21:14:42 +0700

    Add modal: Company

---
+++ src/main/java/com/example/resource/modal/Company.java
@@ -0,0 +1,9 @@
+package com.example.resource.modal;
+
+import org.springframework.data.annotation.Id;
+
+public class Company {
+    private Integer id;
+    private String name;
+    private String email;
+}
